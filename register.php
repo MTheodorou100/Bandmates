4cc2573bcfd5301a3946919e0be7d8fa4bb43bf2
Owen <s3492611@student.rmit.edu.au>
2020-08-11 16:22:45 +1000

    sorry

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username_err = "Please enter a username.";
     } else{
         // Prepare a select statement
-        $sql = "SELECT username FROM users WHERE username = ?";
+        $sql = "SELECT username FROM Person WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
