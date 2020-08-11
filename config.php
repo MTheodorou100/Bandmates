850f15d135041e27773977ee639e27571396f8d9
Owen <s3492611@student.rmit.edu.au>
2020-08-11 14:38:01 +1000

    reisgter we need to host

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'progdatabase-1.ctxbjl62xf2l.us-east-2.rds.amazonaws.com ');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'mypassword');
-define('DB_NAME', 'demo');
+define('DB_NAME', 'programmingdb');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
