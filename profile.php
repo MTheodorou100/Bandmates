1e6b53626f25d49e6619b64a794d168f56eb64a5
s3723443 <s3723443@student.rmit.edu.au>
2020-08-15 00:56:33 +1000

    checkbox instead placeholder

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@
                     <label for="cars">
                         Choose a genre:
                     </label>
-                    <select id="Genres" name="Genres">
+                    <!-- <select id="Genres" name="Genres">
                         <option value="Rock">
                         Rock
                         </option>
@@ -29,7 +29,15 @@
                         <option value="Techno">
                         Techno
                         </option>
-                    </select>
+                    </select> -->
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label for="vehicle1"> I have a bike</label><br>
+
+                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                    <label for="vehicle2"> I have a car</label><br>
+                    
+                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+                    <label for="vehicle3"> I have a boat</label>
                     <input type="submit">
                 </form>
             </section>
