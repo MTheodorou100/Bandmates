60ba2fbb67e09bede1a2c84b93725c0da09e8a73
s3723443 <s3723443@student.rmit.edu.au>
2020-08-15 00:06:25 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,5 +1,38 @@
 <html>
-<body>
+    <head>
 
-</body>
+    </head>
+    <body>
+        <header>
+            header
+        </header>
+        <nav>
+            nav
+        </nav>
+        <main>
+            <section>
+                <form action="/action_page.php">
+                    <fieldset>
+                    <label for="cars">
+                        Choose a genre:
+                    </label>
+                    <select id="Genres" name="Genres">
+                        <option value="Rock">
+                        Rock
+                        </option>
+                        <option value="Jazz">
+                        Jazz
+                        </option>
+                        <option value="Metal">
+                        Metal
+                        </option>
+                        <option value="Techno">
+                        Techno
+                        </option>
+                    </select>
+                    <input type="submit">
+                </form>
+            </section>
+        </main>
+    </body>
 </html>
