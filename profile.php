5a13dd32ec2186a02d0c3eec0af4a26b9e6596bc
s3723443 <s3723443@student.rmit.edu.au>
2020-08-15 03:41:59 +1000

    cleaned

--- profile.php
+++ profile.php
@@ -15,28 +15,14 @@
                     <label for="cars">
                         Choose a genre:
                     </label>
-                    <!-- <select id="Genres" name="Genres">
-                        <option value="Rock">
-                        Rock
-                        </option>
-                        <option value="Jazz">
-                        Jazz
-                        </option>
-                        <option value="Metal">
-                        Metal
-                        </option>
-                        <option value="Techno">
-                        Techno
-                        </option>
-                    </select> -->
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-                    <label for="vehicle1"> I have a bike</label><br>
-
-                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-                    <label for="vehicle2"> I have a car</label><br>
-
-                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-                    <label for="vehicle3"> I have a boat</label>
+                    <input type="checkbox" id="genre1" value="Rock">
+                    <label for="genre1"> Rock </label>
+                    <br>
+                    <input type="checkbox" id="genre2" value="Jazz">
+                    <label for="genre2"> Jazz </label>
+                    <br>
+                    <input type="checkbox" id="genre3" value="Metal">
+                    <label for="genre3"> Metal </label>
                     <input type="submit">
                 </form>
             </section>
