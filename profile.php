2b72b27f887509e2afd608cd3cd4da7304870741
s3723443 <s3723443@student.rmit.edu.au>
2020-08-15 04:02:12 +1000

    breakline fix

--- profile.php
+++ profile.php
@@ -15,6 +15,7 @@
                     <label for="cars">
                         Choose a genre:
                     </label>
+                    <br>
                     <input type="checkbox" id="genre1" value="Rock">
                     <label for="genre1"> Rock </label>
                     <br>
@@ -23,6 +24,7 @@
                     <br>
                     <input type="checkbox" id="genre3" value="Metal">
                     <label for="genre3"> Metal </label>
+                    <br>
                     <input type="submit">
                 </form>
             </section>
