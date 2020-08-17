9d97f085e8157a4d09261bb7e0277e815d57f068
s3723443 <s3723443@student.rmit.edu.au>
2020-08-17 18:19:46 +1000

    inserted profile elements

--- dProfile.php
+++ dProfile.php
@@ -1,9 +1,41 @@
+
 <html>
-    <head>
-        
-    </head>
-    <body>
-        <main>
+	<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+</style> 
+	<link rel="stylesheet" href="../client/css.css">
+<body>  
+	<?php
+
+session_start();  
+$servername = utf8_encode("35.197.167.52");
+$dbname = utf8_encode("bandmates");
+$username = utf8_encode("root");
+$password = utf8_encode("mypassword");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}     
+
+$fname = $_POST['fname'];
+$lname= $_POST['lname'];
+$instrument= $_POST['instrument'];
+$genre= $_POST['genre'];
+$username= $_SESSION['login_user'];
+
+$sql = "UPDATE Person SET firstName='$_POST[fname]', surName='$_POST[lname]', instrument='$_POST[instrument]', genre='$_POST[genre]' WHERE username='$_SESSION[login_user]'";
+  
+if ($conn->query($sql) === TRUE) {
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
+	<h1>The user has been registered</h1>
+	<p> ToDo Displaying Profile Section</p>
+	
+	<main>
             <section>
                 Username:
                 <div>
@@ -35,5 +67,6 @@
                 </div>
             </section>
         </main>
-    </body>
+
+</body>
 </html>
--- dprofile.php
+++ dprofile.php
@@ -35,5 +35,38 @@ if ($conn->query($sql) === TRUE) {
 	<h1>The user has been registered</h1>
 	<p> ToDo Displaying Profile Section</p>
 	
+	<main>
+            <section>
+                Username:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Name:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Instruments:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Genre Interests:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Account Creation Date:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+        </main>
+
 </body>
 </html>
