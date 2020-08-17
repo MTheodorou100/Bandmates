b312357f201fb64b959760eabacec84e3e3ccf3f
s3723443 <s3723443@student.rmit.edu.au>
2020-08-17 17:52:29 +1000

    dProfile added

---
+++ dProfile.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <main>
+            <section>
+                Username:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Name:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Instruments:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Genre Interests:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+            <section>
+                Account Creation Date:
+                <div>
+                    <!-- fill with php echo -->
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
