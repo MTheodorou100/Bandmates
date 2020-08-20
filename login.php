d6b81cc175df8919cf76ef462c5999a03a9544a7
Charlotte Burns <s3645695@student.rmit.edu.au>
2020-08-20 21:27:03 +1000

    Update login.php

--- login.php
+++ login.php
@@ -55,11 +55,11 @@
   <h1 class="h3 mb-3 font-weight-normal">Bandmates  |  Login</h1>
 <div class="email">
   <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <input type="email" id="inputEmail" class="form-control">
 </div>
 <div class="password">
   <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <input type="password" id="inputPassword" class="form-control">
 </div>
   <button class="button-login" type="submit">Login</button> or <button class="button-register" type="submit">Register</button>
 </form>
