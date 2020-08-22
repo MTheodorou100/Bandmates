3c4e2c72e9aa5f4e033d4c41417345a121e94830
Charlotte Burns <s3645695@student.rmit.edu.au>
2020-08-22 12:59:05 +1000

    updated with bootstrap

--- eprofile.php
+++ eprofile.php
@@ -1,56 +1,64 @@
             <?php   
             session_start();         
             ?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" type="text/css" href="style.css"> 
-        <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
-    </head>
 
-    <body>
-        <header>
-            Bandmates Profile
-        </header>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Bandmates | Create a Profile</title>
 
-        <nav>
-           <a href="home.html">Home </a>
-           <a href="register.html"> Register </a>
-           <a href="bands.html"> Bands</a>
-        </nav>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
 
-        <main>
-            <section>
-                <form action="dprofile.php" method='POST'>
-                    First Name
-                    <br>
-                    <input type="text" name="fname">
-                    <br>
-                    Last Name
-                    <br>
-                    <input type="text" name="lname">
-                    <br>
-                    Favourite Instrument
-                    <br>
-                    <select name="instrument" id='instrument'>
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  
+
+    <!-- Custom styles for this template -->
+    <link href="register.css" rel="stylesheet">
+  </head>
+
+        <body class="text-center">
+   		 <form class="form-register" action="dprofile.php" method='POST'>
+ 		 <h1 class="h3 mb-3 font-weight-normal">Create a Profile</h1>
+
+<div class="fname">
+  <label for="firstname" class="sr-only">First Name</label>
+  <input type="text" id="inputFName" class="form-control">
+</div>
+
+<div class="lname">
+  <label for="lastname" class="sr-only">Last Name</label>
+  <input type="text" id="inputLName" class="form-control">
+</div>
+
+<div class="instrument">
+  <label for="instrumentSelect" class="sr-only">Instrument Played</label>
+	<select name="instrument" id='instrument'>
                     <option value="Guitar">Guitar</option>
                     <option value="Bass Guitar">Bass Guitar</option>
                     <option value="Drums">Drums</option>
-                    </select>
-                    <br>
-                    Favourite Genre
-                    <br>
-                    <select name="genre" id='genre'>
+                    <option value="Vocals">Vocals</option>
+    </select>
+</div>
+
+<div class="genre">
+  <label for="genreSelect" class="sr-only">Preferred Genre</label>
+	<select name="genre" id='genre'>
                     <option value="Rock">Rock</option>
                     <option value="Jazz">Jazz</option>
                     <option value="Metal">Metal</option>
-                    </select>
-                    <br>
-                    <br>
-                    <input type="submit">
-                </form>
-            </section>
-        </main>
+                    <option value="RnB">RnB</option>
+    </select>
+</div>
+                           
+ <button class="button-register" type="submit">Register</button>
+         
+         </form>
     </body>
 </html>
