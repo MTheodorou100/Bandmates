a8e1786999928ef205fac146ee9913e469afbdd5
s3723443 <s3723443@student.rmit.edu.au>
2020-09-20 05:35:17 +1000

    fixed sql loops

--- feed.php
+++ feed.php
@@ -106,32 +106,95 @@
         <img src="assets/img/intro-img.svg" alt="" class="img-fluid">
       </div>
         <div class='intro-info' data-aos='zoom-in' data-aos-delay='100'>
+		
   
-        <?php
-  if($db == false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-  }
+<?php
+	if($db == false)
+	{
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
 
-  $item = ($_GET['item']);    //change to the user's current preference (as set in their profile edit)
-  $item = ("guitar");
+	$sqlPI = "SELECT prefInstrument 
+	FROM Person 
+	WHERE username = '{$_SESSION['login_user']}'";
+	$resultPI = mysqli_query($db, $sqlPI) or die(mysqli_error($db)); 
 
-  $sql = "SELECT * FROM Person WHERE instrument LIKE '%".$item."%' OR genre LIKE '%".$item."%'";
-  $result = mysqli_query($db, $sql) or die(mysqli_error($db));
-  
+	$sqlPG = "SELECT prefGenre 
+	FROM Person 
+	WHERE username = '{$_SESSION['login_user']}'";
+	$resultPG = mysqli_query($db, $sqlPG) or die(mysqli_error($db));
 
-  if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-      $results = "Name: " . $row["firstName"]. " " . $row["surName"]. " " . $row["instrument"]. " " . $row["genre"]. "<br>";
-      echo "<div>";
-      echo $results;
-      echo "</div>";
-    }
-  } else {
-    echo "0 results";
-  }
-  
-  $db->close();
+	// echo $_SESSION['login_user'];
+	// echo "<br>";
+	// echo $sqlPI;
+	// echo "<br>";
+	// echo $resultPI;
+	// echo "<br>";
+	// echo "<br>";
+	if (mysqli_num_rows($resultPI) > 0) 
+	{
+		while($row = mysqli_fetch_array($resultPI, MYSQL_ASSOC)) 
+		{
+			$resultsPI = "prefInstrument = " . $row["prefInstrument"]. "<br>";
+			echo "<div style=\"color:white\">";
+			// echo "resultsPI= ";
+			echo $resultsPI;
+
+			// echo $row["prefInstrument"];
+
+			echo "</div>";
+		}
+	} 
+	else 
+	{
+		echo "0 results for resultPI";
+	}
+
+	echo $sqlPG;
+	echo "<br>";
+	// echo "<div>testdiv</div>";
+
+	// $item = ($_GET['item']);    //change to the user's current preference (as set in their profile edit)
+	// $item = ("guitar");
+
+	// $sql = "SELECT * FROM Person WHERE instrument LIKE '%".$item."%' OR genre LIKE '%".$item."%'";
+	// $resultp = mysqli_query($db, $sql) or die(mysqli_error($db));
+
+
+	if (mysqli_num_rows($resultPI) > 0) 
+	{
+		// output data of each row
+		while($row = mysqli_fetch_array($resultPI, MYSQL_ASSOC)) 
+		{
+			$resultsPI = "Name: " . $row["firstName"]. " " . $row["surName"]. " " . $row["instrument"]. " " . $row["genre"]. "<br>";			//needs to be changed from the current user to searching the entire database for the current users preference
+			echo "<div>";
+			echo $resultsPI;
+			echo "</div>";
+		}
+
+	} 
+	else 
+	{
+		echo "0 results";
+	}
+
+	if (mysqli_num_rows($resultPG) > 0) 
+	{
+		while($row = mysqli_fetch_array($resultPG, MYSQL_ASSOC)) 
+		{
+			$resultsPG = "Name: " . $row["firstName"]. " " . $row["surName"]. " " . $row["instrument"]. " " . $row["genre"]. "<br>";
+			echo "<div>";
+			echo $resultsPG;
+			echo "</div>";
+		}
+	}
+	else 
+	{
+		echo "0 results";
+	}
+
+
+	$db->close();
 ?>
           
  
