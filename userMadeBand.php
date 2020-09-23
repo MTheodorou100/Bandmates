c9c2667d46fb6f74ec2bf3e82c2b54d654ed414c
s3723443 <s3723443@student.rmit.edu.au>
2020-09-24 04:28:01 +1000

    some improvements to the form and post vars

--- userMadeBand.php
+++ userMadeBand.php
@@ -1,10 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
     <body>
-        SHOULD ONLY LOAD IF USER IS LOGGED IN AND IN SESSION
+        <h1>
+            userMadeBand Page
+        </h1>
+        <h2>
+            (SHOULD ONLY LOAD IF USER IS LOGGED IN AND IN SESSION)
+        </h2>
+        <?php
+            session_start();  
+            $servername = utf8_encode("35.197.167.52");
+            $dbname = utf8_encode("bandmates");
+            $username = utf8_encode("root");
+            $password = utf8_encode("mypassword");
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) 
+            {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            
+            $newBandName = $_POST['bandName'];
+            $newBandGenre = $_POST['bandGenre'];
+            $newBandLeader = $_POST['bandLeader'];
+            echo "<br> bandName = " . $newBandName . "<br> bandGenre = " . $newBandGenre . "<br> bandLeader = " . $newBandLeader . "<br>";
+            
+            // $_SESSION['login_user'] = "el poopy";
 
-        <?php 
-            // $_SESSION['login_user'] = "poop" //uncomment to test echo in form
+            if ( $_SESSION['login_user']==null)
+            {
+                echo "You must be logged in to make a band";
+            }
+            else
+            {
+                echo "you're logged in, FORM GOES HERE";
+            }
         ?>
 
         <form action="" method="post"> 
@@ -16,6 +45,8 @@
             <br>
             <label>Band Leader</label>
             <input name="BandLeader" type="text" value="<?php echo $_SESSION['login_user'] ?>" required readonly>
+            <br>
+            <input type="submit">
         </form>
     </body>
 </html>
