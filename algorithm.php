811af4527af7a2ccf1086c3b81892a3ec4972be4
Mike <mickeybitey@hotmail.com>
2020-10-07 22:12:25 +1100

    algorith step 2

--- algorithm.php
+++ algorithm.php
@@ -1,6 +1,5 @@
 <html>
     <body>
-    <table>
          <style>
 table {
   font-family: arial, sans-serif;
@@ -50,7 +49,7 @@ form {
             $userArray[$loopValA][1] = $row['firstName'];
             $userArray[$loopValA][2] = $row['surName'];
             $userArray[$loopValA][3] = $row['username'];
-            
+
             //Echo the Results just for Testing
 //            echo "<tr>";
 //            echo "<th>".$userArray[$loopValA][0]."</th>";
@@ -60,26 +59,48 @@ form {
 //            echo "</tr>";
 //            
             $loopValA++;
-            }?>    
-        </table>
-        <?php
+            }
+     
         //Step 2: Genre score loop (from 0 to 100)
         
-        //Get Number of Band Genres
-        $numBandGenres = "SELECT COUNT(genreID) FROM BandGenres WHERE bandID = 21;";
-        $BandsListResult = mysqli_query($db, $sqlUsersList) or die(mysqli_error($db));
         
+        
+        //Get Number of Band Genres
+        $usersListResult = mysqli_query($db, $sqlUsersList) or die(mysqli_error($db));
+        $numBandGenres = "SELECT COUNT(genreID) FROM BandGenres WHERE bandID = 21;";
+        $numBandGenreResult = mysqli_query($db, $numBandGenres) or die(mysqli_error($db));
+        $v3 = mysqli_fetch_array($numBandGenreResult, MYSQL_ASSOC);
         $loopValB=0;
+
          while ($rowB = mysqli_fetch_array($usersListResult, MYSQL_ASSOC)){
+             //Retrieve Number of Player Genres
              $numPlayerGenres = "SELECT COUNT(genreID) FROM LikedGenres WHERE personID = ".$rowB['personID'].";";
-             $numMatches = "SELECT COUNT(genreID) FROM BandGenres WHERE genreID IN (SELECT genreID FROM LikedGenres WHERE personID = ".$rowB['personID']." )";
-             $genreScore = $numMatches / (($numPlayerGenres+$BandsListResult)-$numMatches);
+             $numPlayerGenresResult = mysqli_query($db, $numPlayerGenres) or die(mysqli_error($db));
+             $v1 = mysqli_fetch_array($numPlayerGenresResult, MYSQL_ASSOC);
              
-             $userArray [$loopValB][4] = $genreScore;
+             //Retrieve Number of Matches
+             $numMatches = "SELECT COUNT(genreID) FROM BandGenres WHERE genreID IN (SELECT genreID FROM LikedGenres WHERE personID = ".$rowB['personID']." )";
+             $numMatchesResult = mysqli_query($db, $numMatches) or die(mysqli_error($db));
+             $v2 = mysqli_fetch_array($numMatchesResult, MYSQL_ASSOC);
+             
+             //Generate Genre Score
+             //$genreScore = $numMatchesResult / (($numPlayerGenres+$numBandGenreResult)-$numMatchesResult);
+             $genreScore = $v2['COUNT(genreID)'] / (($v1['COUNT(genreID)']+$v3['COUNT(genreID)'])-$v2['COUNT(genreID)']);
+             $userArray[$loopValB][4] = $genreScore;
+             
+             //EchoTest
+             echo $userArray[$loopValB][4];
+             echo '<br>';
+//             echo '<br>';
+//             echo $v1['COUNT(genreID)'];
+//             echo '<br>';
+//             echo $v2['COUNT(genreID)'];
+//             echo '<br>';
+//             echo $v3['COUNT(genreID)'];
+//             
              $loopValB++;
          }
         
         ?>
-        
         </body>
     </html>  
