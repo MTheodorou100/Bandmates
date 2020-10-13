ace805c3b05500b38ed41d63ca759114c60b4c15
Owen <s3492611@student.rmit.edu.au>
2020-10-13 11:53:35 +1100

    final fixes

--- index.php
+++ index.php
@@ -20,6 +20,11 @@
 
           <a href='register.php' class='btn-services scrollto'>Click Here to Register</a>
         </div>";
+
+ } else {
+   echo "<h1>Welcome to Band mates!</h1><br><br>";
+   echo "<h3>If you want to start a band click <a class='btn btn-primary' href='makeBand.php' role='button'>here.</a></h3><br>";
+   echo "<h3>If you want to find the perfect band for you, click <a class='btn btn-info' href='bandsThatMatchYou.php' role='button'>here.</a></h3><br>";
                     
 }?>
           
